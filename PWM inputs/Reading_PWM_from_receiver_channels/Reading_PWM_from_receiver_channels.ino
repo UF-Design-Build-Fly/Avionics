281efadb2fcd3446c6f31ae30115a7b7199446ab
ereussow <ereussow@ufl.edu>
2021-04-07 01:23:36 -0400

    Update Reading_PWM_from_receiver_channels.ino

--- PWM inputs/Reading_PWM_from_receiver_channels/Reading_PWM_from_receiver_channels.ino
+++ PWM inputs/Reading_PWM_from_receiver_channels/Reading_PWM_from_receiver_channels.ino
@@ -1,14 +1,34 @@
-double channel[6]; //Channels for throttle, aileron, elevator, rudder, and two others
-byte throttlePin = 5; //However, this test will only use 1 channel.
+//Servo library is used to output "PWM" signals
+#include <Servo.h>
+double channel[3]; //Possible channels we could be reading from.
+bool state = 0; //State keeps track when and when not to use attach() and detach().
+
+//G0 reads channel
+//G1 outputs signal
+
+Servo myServo;
 
 void setup() {
-  pinMode(throttlePin, INPUT); //Wire connecting signal from throttle channel to pin on board
-  Serial.begin(9600);
+  pinMode(G0, INPUT);
 }
 
 void loop() {
 
-  channel[0] = pulseIn(throttlePin, HIGH); //This setup reads the PWM signal from the throttle channel
-  Serial.println(channel[0]);
-
+  channel[0] = pulseIn(G0, HIGH); //Reads PWM from receiver channel on G0 pin
+  if (channel[0] < 1250){ //If PWM signal from channel is less than 1250 microseconds, run code
+    
+    if (state == 0) { //Condition to output signal on G1 pin
+      myServo.attach(G1);
+   }
+   
+   myServo.write(40); //In testing, 40 degree angle resulted in 1ms signal. 
+   state = 1;
+  }
+  else {
+    
+    if (state == 1) { //Condition to stop outputting signals
+      myServo.detach();
+      state = 0; //
+   }
+  }
 }
